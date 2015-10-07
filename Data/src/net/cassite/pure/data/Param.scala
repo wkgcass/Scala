694dcbe9d4591c6d8e4adbf6290e482dba80b006
wkgcass <wkgcass@hotmail.com>
2015-10-07 22:54:52 +0800

    build

--- Data/src/net/cassite/pure/data/Param.scala
+++ Data/src/net/cassite/pure/data/Param.scala
@@ -4,8 +4,11 @@ package net.cassite.pure.data
  * Created by wkgcass on 15/10/7.
  */
 object Param {
-  def apply()=new QueryParameter
-  def limit(start:Int, end :Int)=new QueryParameter().limit(start, end)
-  def orderBy(data:Data[_])=new QueryParameter().orderBy(data)
-  def top(i:Int)=new QueryParameter().top(i)
+  def apply() = new QueryParameter
+
+  def limit(start: Int, end: Int) = new QueryParameter().limit(start, end)
+
+  def orderBy(order: OrderBase*) = new QueryParameter().orderBy(order.toArray: _*)
+
+  def top(i: Int) = new QueryParameter().top(i)
 }
